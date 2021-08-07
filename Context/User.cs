a01cdf588e5ea263bf737da55908707259d83cef
cab <kcaballero@codestack.co>
2021-08-06 21:39:54 -0700

    initial

---
+++ Context/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace updatedLoginApi_1.Context
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string NameFirst { get; set; }
+        public string NameLast { get; set; }
+        public string Email { get; set; }
+        public string HashPassword { get; set; }
+        public string Salt { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ Context/userContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace updatedLoginApi_1.Context
+{
+    public partial class userContext : DbContext
+    {
+        public userContext()
+        {
+        }
+
+        public userContext(DbContextOptions<userContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:newlogin.database.windows.net,1433;Initial Catalog=newlogin;Persist Security Info=False;User ID=caballero08;Password=caballer0.;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("users");
+
+                entity.Property(e => e.CreatedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.HashPassword)
+                    .IsRequired()
+                    .HasMaxLength(500);
+
+                entity.Property(e => e.NameFirst)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.NameLast)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.Salt)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
